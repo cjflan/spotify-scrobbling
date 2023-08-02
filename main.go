1dc47f38098b95c23f5bff5baadb0c0dda80195f
cjflan <89868659+cjflan@users.noreply.github.com>
2023-08-02 12:27:45 -0500

    randomize state and move callback to main

--- main.go
+++ main.go
@@ -1,24 +1,76 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
-	"github.com/cjflan/spotify-scrobbling/controllers"
+	spotifyauth "github.com/cjflan/spotify-scrobbling/auth"
+	"github.com/cjflan/spotify-scrobbling/spotify"
+)
+
+var (
+	auth  = spotifyauth.New(spotifyauth.WithScopes(spotifyauth.ScopeUserReadCurrentlyPlaying))
+	ch    = make(chan *spotify.Client)
+	state = RandomString(16)
 )
 
 func main() {
-	fmt.Print("Hello World")
-	http.HandleFunc("/callback", controllers.Callback)
+
+	if os.Getenv("SPOTIFY_ID") == "" {
+		log.Fatal("SPOTIFY_ID not set as enviornment variable")
+	}
+
+	if os.Getenv("SPOTIFY_SECRET") == "" {
+		log.Fatal("SPOTIFY_SECRET not set as enviornment variable")
+	}
+
+	fmt.Println("Hello World")
+	http.HandleFunc("/callback", callback)
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Println("Got request for:", r.URL.String())
 	})
 	go func() {
-		fmt.Print("Starting Server!")
-		err := http.ListenAndServe("localhost:6669", nil)
+		fmt.Println("Starting Server!")
+		err := http.ListenAndServe("localhost:8080", nil)
 		if err != nil {
 			log.Fatal(err)
 		}
 	}()
+
+	url := auth.AuthURL(state)
+	fmt.Println("Please log in to Spotify by visiting the following page in your browser:", url)
+
+	client := <-ch
+
+	currentlyPlaying, err := client.GetCurrentlyPlaying(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if currentlyPlaying.IsPlaying {
+		fmt.Println("You are currently playing:", currentlyPlaying.Item.Name)
+	} else {
+		fmt.Println("No song playing")
+	}
+}
+
+func callback(w http.ResponseWriter, r *http.Request) {
+	fmt.Println()
+	tok, err := auth.Token(r.Context(), state, r)
+	if err != nil {
+		http.Error(w, "Couldn't get token", http.StatusForbidden)
+		log.Fatal(err)
+	}
+	if st := r.FormValue("state"); st != state {
+		http.NotFound(w, r)
+		log.Fatalf("State mismatch: %s != %s\n", st, state)
+	}
+
+	client := spotify.New(auth.Client(r.Context(), tok))
+	fmt.Fprintf(w, "Login Completed!")
+	ch <- client
+
 }
