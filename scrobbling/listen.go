e97e2675389673acfef47a7eeebdfbcce406709c
cjflan <89868659+cjflan@users.noreply.github.com>
2023-08-25 22:14:24 -0500

    change listen logic

--- scrobbling/listen.go
+++ scrobbling/listen.go
@@ -11,39 +11,46 @@ type stateBag struct {
 	title    string
 	artist   string
 	duration int
+	progress int
+	isPlaying bool
 }
-
-var pollingRate int = 5000 // miliseconds
 
 func (c *Client) Listen() *CurrentlyPlaying {
 	var state stateBag
-	var progress int
 	var playing stateBag
+	var listened *CurrentlyPlaying
 
 	for { 
 		currentlyPlaying, err := c.GetCurrentlyPlaying(context.Background())
 		if err != nil {
 			log.Fatal(err)
 		}
+		if currentlyPlaying == nil {
+			fmt.Println("Nothing Playing")
+			continue
+		}
 		
 		playing = stateBag{
 			title:    currentlyPlaying.Item.Name,
 			artist:   currentlyPlaying.Item.Artists[0].Name,
 			duration: currentlyPlaying.Item.DurationMs,
+			progress: currentlyPlaying.ProgressMs,
+			isPlaying: currentlyPlaying.IsPlaying,
 		}
 
-		if currentlyPlaying.IsPlaying {
-			fmt.Printf("%s - %s\n", currentlyPlaying.Item.Name, currentlyPlaying.Item.Artists[0].Name)
-			progress += pollingRate
-			if progress > playing.duration/2 {
-				return currentlyPlaying
+		if playing.isPlaying {
+			fmt.Printf("%s - %s\n", playing.title, playing.artist)
+			if playing.progress > playing.duration/2 {
+				listened = currentlyPlaying
 			}
 		} else if playing == state {
-			fmt.Printf("%s - %s is paused\n", currentlyPlaying.Item.Name, currentlyPlaying.Item.Artists[0].Name)
+			fmt.Printf("%s - %s is paused\n", playing.title, playing.artist)
 		}
-
+		if listened != nil && state.progress > playing.progress {
+			return listened
+		}
 		state = playing 
-		time.Sleep(time.Millisecond * time.Duration(pollingRate))
+		time.Sleep(time.Second * 5)
 	}
 }
 
